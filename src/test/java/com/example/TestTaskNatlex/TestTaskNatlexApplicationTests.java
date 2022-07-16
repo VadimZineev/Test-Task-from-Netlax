98edb776a430c287b3a87337c4f424863d5505e2
VadimZineev <vadimz026@gmail.com>
2022-07-16 11:38:31 +0300

    Create new project

---
+++ src/test/java/com/example/TestTaskNatlex/TestTaskNatlexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestTaskNatlex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskNatlexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
